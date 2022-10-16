c08f3e6ef973d0759d87d9703d0aabc137529a7f
vyuhovaas <115576913+vyuhovaas@users.noreply.github.com>
2022-10-16 22:29:11 +0300

    Лабораторная работа 3

---
+++ Лабораторная работа 3/task.1.py
@@ -0,0 +1,9 @@
+src = not False and True or False and not True
+
+src = True or False and False
+src = True or False
+src = True# TODO расписать упрощение выражения
+
+result = True
+
+print(src == result)
