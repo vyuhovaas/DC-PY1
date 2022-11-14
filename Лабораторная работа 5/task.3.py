1d19401bdcffd5a3fd69df8053911aa99e181aa6
vyuhovaas <115576913+vyuhovaas@users.noreply.github.com>
2022-11-14 14:50:13 +0300

    Лабораторная работа 5
    исправила указанные ошибки))

---
+++ Лабораторная работа 5/task.3.py
@@ -0,0 +1,13 @@
+import random
+
+
+def get_unique_list_numbers() -> list[int]:
+    start = -10
+    stop = 10
+    n = 15
+    return random.sample(range(start, stop), n)
+
+
+list_unique_numbers = get_unique_list_numbers()
+print(list_unique_numbers)
+print(len(list_unique_numbers) == len(set(list_unique_numbers)))
